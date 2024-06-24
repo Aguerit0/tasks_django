4156e61849027b8e616ad172ca3768334ab7aaed
AgüeroEsteban <cuentauniversidading@gmail.com>
2024-06-24 11:24:54 -0300

    last commit

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,13 @@
-from django.forms import ModelForm
 from .models import Task
+from django import forms
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task # modelo que se va a usar para validar los campos
         fields = ['title', 'description', 'important'] # mecanismo de validacion de campos
+        widgets = {
+            'tittle': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escriba una tarea'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escriba una descripcion'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input text-center m-auto'}),
+            
+        }
